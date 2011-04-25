57342e42d6c9bd509fda9af6bb9b665a70be2755
Scott Fines <scottfines@gmail.com>
2011-04-25 13:18:31 -0500

    Revert "Removes CountDownLatch from DefaultZkSessionManager to address issue #3"
    This reverts commit 6e39b462f4550015fc5eacb3f487fe953be1e304.

--- src/org/menagerie/DefaultZkSessionManager.java
+++ src/org/menagerie/DefaultZkSessionManager.java
@@ -25,6 +25,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -138,13 +139,19 @@ public class DefaultZkSessionManager implements ZkSessionManager{
             }
         }else{
             //make sure that your zookeeper instance is synced
+            final CountDownLatch latch = new CountDownLatch(1);
             zk.sync("/",new AsyncCallback.VoidCallback() {
                 @Override
                 public void processResult(int rc, String path, Object ctx) {
-//                    latch.countDown();
-                    //do nothing, we're good
+                    latch.countDown();
                 }
             }, this);
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                //shouldn't be interruptible, so just continue on, resetting the interrupt status
+                Thread.currentThread().interrupt();
+            }
         }
         return zk;
     }
